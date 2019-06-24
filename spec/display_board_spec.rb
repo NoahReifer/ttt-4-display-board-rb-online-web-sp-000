275a430307db76aeee1b6c5b79a28b6736cccfc3
Noah Reifer <sreifer4@gmail.com>
2019-06-24 19:26:57 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -187,7 +187,11 @@ board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
-
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" 0 | 0 | 0 ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" 0 | 0 | 0 ")
       # *** Comment the line below by adding a # at the line start ***
       #expect(true).to be(true)
     end
